8c104530dcc8e2d3b3ce5dfcd3f34627ea55c380
djhedges <djhedges@gmail.com>
2019-12-23 05:11:37 +0000

    Denote the lap number when reading logs.

--- read_log.py
+++ read_log.py
@@ -6,6 +6,9 @@ import log_files
 for arg in sys.argv[1:]:
   print arg
   session = log_files.ReadLog(arg)
+  lap_num = 1
   for lap in session.laps:
+    print 'Lap: %d' % lap_num
+    lap_num += 1
     for point in lap.points:
       print point
