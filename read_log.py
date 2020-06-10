d8b2d95ea6551b033172a2f4868f71b06c7deee9
djhedges <djhedges@gmail.com>
2020-06-10 15:11:18 -0700

    Update readlog to py3

--- read_log.py
+++ read_log.py
@@ -1,14 +1,14 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import sys
 import log_files
 
 for arg in sys.argv[1:]:
-  print arg
+  print(arg)
   session = log_files.ReadLog(arg)
   lap_num = 1
   for lap in session.laps:
-    print 'Lap: %d' % lap_num
+    print(f'Lap: {lap_num}')
     lap_num += 1
     for point in lap.points:
-      print point
+      print(point)
