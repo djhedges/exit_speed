bd248adde3ffdc366a38d2da32e2017f33cae39a
djhedges <djhedges@gmail.com>
2020-06-11 08:56:39 -0700

    full run time comment

--- find_closest.py
+++ find_closest.py
@@ -26,7 +26,29 @@ def LoadSession():
 class BruteForce(object):
   """Suprisingly not terriable despite O(n^2).
 
-  After a few laps the timing was min 0.03, max 0.06 and median 0.04 seconds.
+  Lap: 1, num of points: 5853
+  Lap: 2, num of points: 3844
+  Lap: 3, num of points: 3730
+  Lap: 4, num of points: 3658
+  Lap: 5, num of points: 3620
+  Lap: 6, num of points: 3634
+  Lap: 7, num of points: 3658
+  Lap: 8, num of points: 3657
+  Lap: 9, num of points: 3656
+  Lap: 10, num of points: 3662
+  Lap: 11, num of points: 3656
+  Lap: 12, num of points: 3672
+  Lap: 13, num of points: 3672
+  Lap: 14, num of points: 3709
+  Lap: 15, num of points: 3690
+  Lap: 16, num of points: 3689
+  Lap: 17, num of points: 3687
+  Lap: 18, num of points: 3674
+  Lap: 19, num of points: 3696
+  Lap: 20, num of points: 5392
+  min - 0.03502225875854492
+  max - 0.08478713035583496
+  median - 0.03641939163208008
   """
   session = attr.ib()
   best_lap = attr.ib(init=False, default=None)
