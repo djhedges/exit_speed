4685c601ee82317d90fc4add18b0721a708b5fd2
djhedges <djhedges@gmail.com>
2020-08-23 16:47:19 -0700

    Add a geohash metric for the grafana worldmap plugin

--- metric_exporter.py
+++ metric_exporter.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import logging
+import geohash
 from multiprocessing import Process
 from multiprocessing import Queue
 from prometheus_client import CollectorRegistry
@@ -13,7 +14,10 @@ METRIC_LAT = Gauge('lat', 'latitude position', registry=REGISTRY)
 METRIC_LON = Gauge('lon', 'longitude position', registry=REGISTRY)
 METRIC_ALT = Gauge('alt', 'altitude', registry=REGISTRY)
 METRIC_SPEED = Gauge('speed', 'GPS reported speed in m/s', registry=REGISTRY)
-
+METRIC_GEOHASH = Gauge('geohash', 'Geohash of the latest GPS point',
+                       ['geohash', 'target'], registry=REGISTRY)
+METRIC_POINTS_EXPORTED = Counter(
+  'points_exported', 'Number of points exported', registry=REGISTRY)
 METRIC_POINTS_SKIPPED = Counter(
   'points_skipped', 'Number of points skipped', registry=REGISTRY)
 
@@ -25,7 +29,7 @@ def _EmptyQueue(queue):
   in case this process gets backed up.
   """
   qsize = queue.qsize()
-  if qsize:
+  if qsize > 1:
     skipped_points = qsize -1
     METRIC_POINTS_SKIPPED.inc(skipped_points)
     logging.info('Metric exporter skipped %d points', skipped_points)
@@ -41,7 +45,10 @@ def PushMetrics(point):
   METRIC_LON.set(point.lon)
   METRIC_ALT.set(point.alt)
   METRIC_SPEED.set(point.speed)
+  geo_hash = geohash.encode(point.lat, point.lon)
+  METRIC_GEOHASH.labels(geohash=geo_hash, target='exit_speed').set(1)
   push_to_gateway('server:9091', job='exit_speed', registry=REGISTRY)
+  METRIC_POINTS_EXPORTED.inc()
 
 
 def Loop(queue):
