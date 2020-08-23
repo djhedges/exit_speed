838b1d8523beeb5709351ed3814d3022552a950f
djhedges <djhedges@gmail.com>
2020-08-22 22:43:15 -0700

    Drop temp _test csv path

--- convert_data_to_csv.py
+++ convert_data_to_csv.py
@@ -11,6 +11,6 @@ if __name__ == '__main__':
   file_path = sys.argv[1]
   es = replay_data.ReplayLog(file_path)
   session = es.GetSession()
-  new_path = '%s_test.csv' % file_path
+  new_path = '%s.csv' % file_path
   print('%s > %s' % (file_path, new_path))
   csv_lib.ConvertProtoToTraqmate(session, new_path)
