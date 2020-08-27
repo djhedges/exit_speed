9bd8a3c6f7f40f25b980489883485457b9e2a5f9
djhedges <djhedges@gmail.com>
2020-08-26 21:18:24 -0700

    Swtich from influxdb > timescale

--- metric_exporter.py
+++ metric_exporter.py
@@ -3,9 +3,9 @@
 import logging
 import queue
 import geohash
+import psycopg2
 from multiprocessing import Process
 from multiprocessing import Queue
-from influxdb import InfluxDBClient
 
 
 class Pusher(object):
@@ -17,8 +17,10 @@ class Pusher(object):
     self.backlog_point_queue = queue.LifoQueue()
     self.lap_queue = Queue()
     self.process = Process(target=self.Loop, daemon=True)
-    self.point_exported = 0  # Incrementing counter of points exported.
+    self.points_exported = 0  # Incrementing counter of points exported.
     self.points_skipped = 0
+    self.first_point_time = None
+    self.current_lap = 1
 
   def GetPointFromQueue(self):
     """Returns the latest point to export metrics for.
@@ -32,47 +34,55 @@ class Pusher(object):
       _ = self.point_queue.get()
     return self.point_queue.get()
 
-  def GetExporterMetrcis(self):
-    return {'measurement': 'metric_exporter',
-            'fields': {'points_skipped': self.points_skipped,
-                       'points_exported': self.point_exported,
-                      },
-           }
+  def ExportProcMetrics(self):
+    with self.timescale_conn.cursor() as cursor:
+        insert_statement = """
+        INSERT INTO metric_exporter (time, points_exported, points_skipped)
+        VALUES (NOW(), %s, %s)
+        """
+        args = (self.points_exported, self.points_skipped)
+        cursor.execute(insert_statement, args)
 
-  def GetPointMetric(self, point):
-    self.point_exported += 1
+  def ExportPointMetric(self, point):
+    self.points_exported += 1
     geo_hash = geohash.encode(point.lat, point.lon, precision=24)
-    return {'measurement': 'point',
-            'fields': {'alt': point.alt,
-                       'speed': point.speed * 2.23694, # m/s to mph.
-                       'geohash': geo_hash,
-                      },
-            'time': point.time.ToJsonString(),
-            'tags': {'lap_number': point.lap_number},
-           }
+    with self.timescale_conn.cursor() as cursor:
+      if point.lap_number != self.current_lap or not self.first_point_time:
+        self.first_point_time = point.time.ToMilliseconds()
+        self.current_lap = point.lap_number
 
-  def GetLapMetric(self, point, lap):
+      elapsed_duration = point.time.ToMilliseconds() - self.first_point_time
+      # timescale
+      insert_statement = """
+      INSERT INTO points (time, alt, speed, geohash, elapsed_duration, lap_number)
+      VALUES (%s, %s, %s, %s, %s, %s)
+      """
+      args = (point.time.ToJsonString(),
+              point.alt,
+              point.speed * 2.23694, # m/s to mph.
+              geo_hash,
+              elapsed_duration,
+              point.lap_number)
+      cursor.execute(insert_statement, args)
+
+  def ExportLapMetric(self, point, lap):
     if lap:
-      lap_point = lap.points[-1]
-      milliseconds = lap.duration.ToMilliseconds()
-      minutes = milliseconds // 60000
-      seconds = milliseconds % 60000 / 1000
-      duration = '%d:%.3f' % (minutes, seconds)
-      return {'measurement': 'lap',
-              'fields': {'lap_number': point.lap_number,
-                         'duration': duration,
-                        },
-              'time': lap_point.time.ToJsonString(),
-             }
+      lap_point = lap.points[0]
+      with self.timescale_conn.cursor() as cursor:
+        insert_statement = """
+        INSERT INTO laps (time, lap_number, duration_ms)
+        VALUES (%s, %s, %s)
+        """
+        args = (lap_point.time.ToJsonString(),
+                lap_point.lap_number,
+                lap.duration.ToMilliseconds())
+        cursor.execute(insert_statement, args)
 
   def PushMetrics(self, point, lap):
-    values = []
-    values.append(self.GetPointMetric(point))
-    lap_metric = self.GetLapMetric(point, lap)
-    if lap_metric:
-      values.append(lap_metric)
-    values.append(self.GetExporterMetrcis())
-    self.influx_client.write_points(values)
+    self.ExportPointMetric(point)
+    self.ExportLapMetric(point, lap)
+    self.ExportProcMetrics()
+    self.timescale_conn.commit()
 
   def Loop(self):
     while True:
@@ -84,8 +94,8 @@ class Pusher(object):
       self.PushMetrics(point, lap)
 
   def Start(self):
-    self.influx_client = InfluxDBClient(
-        'server', 8086, 'root', 'root', 'exit_speed')
+    self.timescale_conn = psycopg2.connect(
+        'postgres://postgres:postgres@server:/exit_speed')
     self.process.start()
 
 
