6e23f2374afa1a970308edc198f59d21952a40f4
djhedges <djhedges@gmail.com>
2020-09-13 15:06:09 -0700

    Switch to fake pi on exit_speed_test

--- exit_speed_test.py
+++ exit_speed_test.py
@@ -17,11 +17,11 @@ import unittest
 import mock
 from absl import flags
 from absl.testing import absltest
-# Fixes dotstar import on Travis.
-import adafruit_platformdetect
-with mock.patch.object(adafruit_platformdetect, 'Detector') as mock_detector:
-  mock_detector.chip.id.return_value = 'BCM2XXX'
-  import adafruit_dotstar
+import fake_rpi
+sys.modules['RPi'] = fake_rpi.RPi     # Fake RPi
+sys.modules['RPi.GPIO'] = fake_rpi.RPi.GPIO # Fake GPIO
+sys.modules['smbus'] = fake_rpi.smbus # Fake smbus (I2C)
+import adafruit_dotstar
 import exit_speed
 import leds
 from gps import client
@@ -33,13 +33,6 @@ FLAGS.set_default('config_path', 'testdata/test_config.yaml')
 
 
 class TestExitSpeed(unittest.TestCase):
-
-  def setUp(self):
-    super(TestExitSpeed, self).setUp()
-    mock_star = mock.create_autospec(adafruit_dotstar.DotStar)
-    patch = mock.patch.object(adafruit_dotstar, 'DotStar')
-    patch.return_value = mock_star
-    patch.start()
 
   def testPointDelta(self):
     point_a = gps_pb2.Point()
