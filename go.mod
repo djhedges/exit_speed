848ffccf379561fd0b32db55ee94b949015a49e9
djhedges <djhedges@google.com>
2020-10-03 14:52:10 -0700

    Try checking in gps.pb.go.  I bet this breaks travis

--- go.mod
+++ go.mod
@@ -2,5 +2,6 @@ module github.com/djhedges/exit_speed
 
 require (
 	github.com/golang/protobuf v1.4.2
+	google.golang.org/grpc v1.27.0
 	google.golang.org/protobuf v1.25.0
 )
--- reflector/reflector.go
+++ reflector/reflector.go
@@ -12,8 +12,36 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+//
+// Golang implementation for exporting data to Timescale.
 package reflector
+
+import (
+	"google.golang.org/grpc"
+	gpspb "github.com/djhedges/exit_speed/gps_go_proto"
+)
 
 func A() string {
   return "test"
+}
+
+type reflect struct {
+	gpspb.UnimplementedReflectServer
+}
+
+func (s *reflect) ExportPoint(ctx context.Context, in *gpspb.PointUpdate) (*gpspb.Response, error) {
+	log.Printf('I haz a point')
+	return &gpspb.Response{}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", 65000)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	gpspb.RegisterReflectServer(s, &reflect{})
+	if err := s.Serve(lis); err != nil {
+		log.Fataf("Failed to serve: %v", err)
+	}
 }
