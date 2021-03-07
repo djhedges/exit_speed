6c291fd5bbce343e60ae118fc2f571591858b752
djhedges <djhedges@google.com>
2021-03-07 15:15:20 -0800

    Switch to controlling the GoPro over bluetooth

--- gopro.py
+++ gopro.py
@@ -12,24 +12,50 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""GoPro camera control."""
+"""GoPro camera control over bluetooth.
 
-import goprocam
+Note enabling WiFi generates more heat and battery consummation.
+
+Some interesting links:
+  https://gethypoxic.com/blogs/technical/gopro-hero5-interfaces
+  https://github.com/KonradIT/goprowifihack/blob/master/Bluetooth/Platforms/RaspberryPi.md
+  https://github.com/KonradIT/gopro-ble-py
+"""
+
+import time
 from absl import app
+import pygatt
+
+COMMAND_UUID = 'b5f90072-aa8d-11e3-9046-0002a5d5c51b'
+RECORD_START = bytearray(b'\x03\x01\x01\x01')
+RECORD_STOP = bytearray(b'\x03\x01\x01\x00')
+ADDRESS = 'E0:86:1C:77:19:59'
 
 
 class GoPro(object):
   """Starts/stops the GoPro recording."""
 
   def __init__(self):
-    self.camera = goprocam.GoProCamera.GoPro()
+    self.adapter = pygatt.GATTToolBackend()
+    self.adapter.start()
+    self.camera = self.adapter.connect(
+        ADDRESS, address_type=pygatt.BLEAddressType.random)
+
+  def _WriteCmd(self, command):
+    self.camera.char_write(COMMAND_UUID, command)
 
   def Start(self):
-    self.camera.shoot_video(10)
+    self._WriteCmd(RECORD_START)
+
+  def Stop(self):
+    self._WriteCmd(RECORD_STOP)
 
 
 def main(unused_argv):
   gopro = GoPro()
+  gopro.Start()
+  time.sleep(5)
+  gopro.Stop()
 
 
 if __name__ == '__main__':
