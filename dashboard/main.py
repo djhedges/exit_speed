6b80374e8c8b0b4e0c887ec97aee235a5bfe8012
djhedges <djhedges@gmaill.com>
2021-12-13 18:32:22 -0800

    A hello world dash app on app engine

--- dashboard/main.py
+++ dashboard/main.py
@@ -12,15 +12,35 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from flask import Flask
+import dash
+from dash import dcc
+from dash import html
+import plotly.express as px
+import pandas as pd
 
-app = Flask(__name__)
+app = dash.Dash(__name__)
+server = app.server
 
-@app.route('/')
-def hello():
-    """Return a friendly HTTP greeting."""
-    return 'Hello World!'
+df = pd.DataFrame({
+    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+    "Amount": [4, 1, 2, 2, 4, 5],
+    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+})
 
+fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
+
+    html.Div(children='''
+        Dash: A web application framework for your data.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    )
+])
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run_server(debug=True)
