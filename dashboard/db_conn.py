77dbe60630547b1645afbcda8c9ca9dd6cbea424
djhedges <djhedges@google.com>
2021-12-25 19:25:16 -0800

    Add API call to start SQL backend if it was down

--- dashboard/db_conn.py
+++ dashboard/db_conn.py
@@ -14,8 +14,10 @@
 """Database connection library."""
 
 import json
+import logging
 import os
 import sqlalchemy
+from googleapiclient import discovery
 
 # Local args used when running a development instance.
 # {'password': '',
@@ -25,6 +27,10 @@ import sqlalchemy
 #  'host': 'ip-address'}
 SECRET_LOCAL_ID = (
     'projects/794720490237/secrets/db-conn-local-args/versions/latest')
+# Obscuring the project id.  This is probably overkill but this code is public.
+SECRET_PROJECT_ID = (
+    'projects/794720490237/secrets/project_id/versions/latest')
+INSTANCE_NAME = 'exit-speed'
 
 
 def _GetSecret(secret_id):
@@ -37,8 +43,25 @@ def _GetSecret(secret_id):
   return json.loads(response.payload.data)
 
 
+def StartInstance():
+  service = discovery.build('sqladmin', 'v1beta4')
+  project = _GetSecret(SECRET_PROJECT_ID)
+  request = service.instances().get(project=project, instance=INSTANCE_NAME)
+  response = request.execute()
+  if response['settings']['activationPolicy'] == 'ALWAYS':
+    logging.info('SQL instance is already up')
+  else:
+    request = service.instances().patch(
+        project=project, instance=INSTANCE_NAME, body={
+            'settings': {'activationPolicy': 'ALWAYS'}})
+    response = request.execute()
+    logging.info('Start SQL instance response: %s', response)
+
+
+
 def InitPool():
   if os.getenv('GOOGLE_CLOUD_PROJECT'):
+    StartInstance()
     local_args = _GetSecret(SECRET_LOCAL_ID)
     return sqlalchemy.create_engine(sqlalchemy.engine.url.URL(**local_args))
   return sqlalchemy.create_engine(sqlalchemy.engine.url.URL(
