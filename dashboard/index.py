186f0abad7fb7cd2b683605b917920c46c3dcd5e
djhedges <djhedges@google.com>
2022-01-05 08:50:27 -0800

    Broken commit at attempt to add URL parsing

--- dashboard/index.py
+++ dashboard/index.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """App Engine dashboard using Dash, Plotly and Pandas."""
+import urllib
+
 import live
 from app import app
 from dash import dcc
@@ -19,7 +21,8 @@ from dash import html
 from dash.dependencies import Input
 from dash.dependencies import Output
 
-app.layout = html.Div([
+
+BASE_HTML = html.Div([
   dcc.Location(id='url', refresh=False),
   dcc.Link('Home', href='/'),
   html.Br(),
@@ -28,12 +31,45 @@ app.layout = html.Div([
   html.Div(id='page-content')
 ])
 
+app.layout = BASE_HTML
+app.validation_layout = html.Div([
+    BASE_HTML,
+    live.LAYOUT,
+])
+
 @app.callback(Output('page-content', 'children'),
               Input('url', 'pathname'))
 def URLRouting(pathname):
-  print(pathname)
   if pathname == '/live/':
     return live.LAYOUT
+
+
+@app.callback(
+  Output('url', 'href'),
+  Input('url', 'href'),
+  Input('points-dropdown', 'value'),
+  prevent_initial_call=True,
+)
+def UpdateURL(href, points):
+  args = {'points': points}
+  return urllib.parse.urljoin(href, urllib.parse.urlencode(args, doseq=True))
+
+
+@app.callback(
+  Output('points-dropdown', 'value'),
+  Input('url', 'pathname'),
+)
+def ParseURL(pathname):
+  # Strip a leading "/" with [1:]
+  params = urllib.parse.parse_qs(pathname[1:])
+  points = params.get(
+              'points',
+              [
+               'speed',
+               'tps_voltage',
+               'front_brake_pressure_percentage',
+               ])
+  return points
 
 
 if __name__ == '__main__':
